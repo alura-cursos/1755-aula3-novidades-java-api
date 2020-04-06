04c1f9857abd1e51c52767f935cef752886ddf1f
João Victor <jvc.martins12@gmail.com>
2020-04-06 15:48:04 -0300

    Suporte ao http 2

--- src/main/java/br/com/alura/novidadesjavarest/NovidadesJavaRestApplication.java
+++ src/main/java/br/com/alura/novidadesjavarest/NovidadesJavaRestApplication.java
@@ -1,13 +1,24 @@
 package br.com.alura.novidadesjavarest;
 
+import org.apache.coyote.http2.Http2Protocol;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
+import org.springframework.boot.web.servlet.server.ConfigurableServletWebServerFactory;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class NovidadesJavaRestApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(NovidadesJavaRestApplication.class, args);
+	}
+
+	@Bean
+	public ConfigurableServletWebServerFactory tomcatCustomizer() {
+		TomcatServletWebServerFactory factory = new TomcatServletWebServerFactory();
+		factory.addConnectorCustomizers(connector -> connector.addUpgradeProtocol(new Http2Protocol()));
+		return factory;
 	}
 
 }
